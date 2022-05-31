8379c540071a17439ddd7f8aa3cccec045ad63e3
kalebeccs <kalebe.correa.santos@gmail.com>
2022-05-31 19:19:24 +0100

    Add project javascript mvc
    Adicionando o projeto desenvolvido com o conceito mvc.

---
+++ javascript-client-mvc/README.md
@@ -0,0 +1,5 @@
+# Projeto Negociações
+
+Arquivos do projeto negociações, focados no uso de javascript usando o padrao mvc.
+
+Link: https://kalebeccs.github.io/learning/javascript-client-mvc/negociacoes/index.html
---
+++ javascript-client-mvc/negociacoes/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,13 @@
+class ListaNegociacoes {
+    constructor() {
+        this._negociacoes = [];
+    }
+
+    add(negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+        return [].concat(this._negociacoes);
+    }
+}
